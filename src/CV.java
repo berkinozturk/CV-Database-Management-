98541e45fa4cbf0f84642e19b7378636224b1b79
Berkin Öztürk <berkin.ozturk@std.izmirekonomi.edu.tr>
2022-12-25 23:55:00 +0300

    database and some part of add function are added

--- src/CV.java
+++ src/CV.java
@@ -1,6 +1,15 @@
+import java.util.Date;
+
 public class CV {
 
-    public static void addCV(){}
+
+    public static void addCV(String name, String surname, String Education, String[] Languages, String[] Experiences, String[] Projects, String Department, String Address, int ID, String[] Competencies, String[] Certificates, Long PhoneNumber, Date LocalDate, String About){
+
+
+        String sql = "CREATE FUNCTION add(name TEXT , y INTEGER) " +
+                "RETURNS INTEGER " +
+                "AS $$ SELECT x + y; $$ LANGUAGE SQL";
+    }
     public static void openCV(){}
     public static void generateCV(){}
     public static void searchCV(){}
--- src/CVManagementSystem.java
+++ src/CVManagementSystem.java
@@ -1,8 +1,38 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CVManagementSystem {
     public static void main(String[] args) {
 
         CV cv = new CV();
         CV cv2 = new CV();
 
+
+
+        try {
+            // Load the SQLite JDBC driver
+            Class.forName("org.sqlite.JDBC");
+
+            // Connect to the database
+            String url = "jdbc:sqlite:/C:\\Program Files\\DB Browser for SQLite\\Tag.db";
+            Connection conn = DriverManager.getConnection(url);
+
+            // Do something with the connection...
+            System.out.println("connected");
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.executeUpdate();
+            conn.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        }
+
+
     }
-}
+
+
+
