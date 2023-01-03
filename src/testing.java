798ca44f123c7c66a7f881e491020a9b7509cfe3
Ege Sevinç <egesevinc@windowslive.com>
2023-01-03 21:11:29 +0300

    delete test

--- src/testing.java
+++ src/testing.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class testing {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import java.sql.*;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+class testing {
+    @Test
+    void testDeleteCV() throws SQLException {
+        int ID = 1;
+        // Call the deleteCV method
+        CV.deleteCV(ID);
+
+        // Verify that the CV was correctly deleted from the database
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try {
+            // Connect to the database
+            conn = DriverManager.getConnection("jdbc:sqlite:Tag.db");
+
+            // Query the database for the test CV
+            String sql = "SELECT COUNT(*) FROM Tag WHERE Id = ?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, ID);
+            ResultSet rs = stmt.executeQuery();
+            int count = rs.getInt(1);
+
+            // Verify that the test CV was not found in the database
+            assertEquals(0, count);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            // Close the connection and statement
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
 }
